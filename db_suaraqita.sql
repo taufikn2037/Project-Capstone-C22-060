7ea7174960d7cb39d1c86333b2a30cc165cdf536
Vinn10 <kevin.hutasoit213@gmail.com>
2022-12-01 15:11:49 +0700

    add sql

--- db_suaraqita.sql
+++ db_suaraqita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Nov 2022 pada 12.00
+-- Waktu pembuatan: 01 Des 2022 pada 09.09
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_suaraqita`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name__admin` varchar(255) NOT NULL,
+  `username__admin` varchar(128) NOT NULL,
+  `password__admin` varchar(255) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admins`
+--
+
+INSERT INTO `admins` (`id`, `name__admin`, `username__admin`, `password__admin`, `role_id`, `is_active`) VALUES
+(1, 'Kevin Jonathan', 'admin_kevin', 'admin123', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -45,8 +67,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name__user`, `email__user`, `username__user`, `nik__user`, `password__user`, `role_id`, `is_active`, `date_created`, `image`) VALUES
-(1, 'Kevin Jonathan', 'kevin.hutasoit213@gmail.com', 'kevin10', 2147483647, '0', 2, 1, 1669653204, 'default.jpg'),
-(3, 'kevin', 'kevin@gmail.com', 'kevin', 2147483647, '$2y$10$VbHRO/9z/sBJybHYSCEYdeyFMwb5MsA/mnTEhmga1GBw7qu3yJ9BW', 2, 1, 1669656125, 'default.jpg');
+(3, 'kevin', 'kevin@gmail.com', 'kevin', 2147483647, '$2y$10$VbHRO/9z/sBJybHYSCEYdeyFMwb5MsA/mnTEhmga1GBw7qu3yJ9BW', 2, 1, 1669656125, 'default.jpg'),
+(4, 'kevin', 'kevin123@gmail.com', 'kevin01', 2147483647, '$2y$10$MOPqIVC6eRCEiF.n389li.jF9yOynzoaTQeAO3n6Tg2zOtuuMNMo.', 2, 1, 1669732208, 'default.jpg'),
+(5, 'Ke Jo', 'petugas1@gmail.com', 'petugas1', 0, '$2y$10$GDSsUd5wg70xy7hqN2J1mOMSFv0Htu2Nwv71ac7Qdux261urS.Lv.', 3, 1, 1669812621, 'default.jpg');
 
 -- --------------------------------------------------------
 
@@ -65,11 +88,18 @@ CREATE TABLE `users_role` (
 
 INSERT INTO `users_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
-(2, 'Member');
+(2, 'Member'),
+(3, 'Petugas');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -88,16 +118,22 @@ ALTER TABLE `users_role`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `users_role`
 --
 ALTER TABLE `users_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
