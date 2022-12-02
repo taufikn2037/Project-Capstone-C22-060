589977da027bcb89f5fc0275a1193baa73e706c9
ordian-7767 <mordiansyah13th@gmail.com>
2022-12-02 19:01:35 +0700

    Improving Beranda Page Web Design

---
+++ assets/dist/js/pages/beranda.js
@@ -0,0 +1,13 @@
+console.log("stupid");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
