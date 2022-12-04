973ed01ad17b7f8c0d3699024654c91d1b15a2ec
taufikn2037 <taufik.mesuji@gmail.com>
2022-12-05 03:44:47 +0700

    update sql

--- db_suaraqita.sql
+++ db_suaraqita.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Des 2022 pada 09.09
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.7
+-- Waktu pembuatan: 04 Des 2022 pada 21.43
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admins` (
-  `id` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
   `name__admin` varchar(255) NOT NULL,
   `username__admin` varchar(128) NOT NULL,
   `password__admin` varchar(255) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `is_active` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -40,8 +40,9 @@ CREATE TABLE `admins` (
 -- Dumping data untuk tabel `admins`
 --
 
-INSERT INTO `admins` (`id`, `name__admin`, `username__admin`, `password__admin`, `role_id`, `is_active`) VALUES
-(1, 'Kevin Jonathan', 'admin_kevin', 'admin123', 3, 1);
+INSERT INTO `admins` (`id_admin`, `name__admin`, `username__admin`, `password__admin`, `id_role`, `is_active`) VALUES
+(1, 'Administrator', 'admin', 'admin123', 1, 1),
+(2, 'Taufik', 'taufik124', '12345', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -50,13 +51,13 @@ INSERT INTO `admins` (`id`, `name__admin`, `username__admin`, `password__admin`,
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `name__user` varchar(255) NOT NULL,
   `email__user` varchar(128) NOT NULL,
   `username__user` varchar(128) NOT NULL,
   `nik__user` int(16) NOT NULL,
   `password__user` varchar(255) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
   `date_created` int(11) NOT NULL,
   `image` varchar(128) NOT NULL
@@ -66,10 +67,8 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name__user`, `email__user`, `username__user`, `nik__user`, `password__user`, `role_id`, `is_active`, `date_created`, `image`) VALUES
-(3, 'kevin', 'kevin@gmail.com', 'kevin', 2147483647, '$2y$10$VbHRO/9z/sBJybHYSCEYdeyFMwb5MsA/mnTEhmga1GBw7qu3yJ9BW', 2, 1, 1669656125, 'default.jpg'),
-(4, 'kevin', 'kevin123@gmail.com', 'kevin01', 2147483647, '$2y$10$MOPqIVC6eRCEiF.n389li.jF9yOynzoaTQeAO3n6Tg2zOtuuMNMo.', 2, 1, 1669732208, 'default.jpg'),
-(5, 'Ke Jo', 'petugas1@gmail.com', 'petugas1', 0, '$2y$10$GDSsUd5wg70xy7hqN2J1mOMSFv0Htu2Nwv71ac7Qdux261urS.Lv.', 3, 1, 1669812621, 'default.jpg');
+INSERT INTO `users` (`id_user`, `name__user`, `email__user`, `username__user`, `nik__user`, `password__user`, `id_role`, `is_active`, `date_created`, `image`) VALUES
+(6, 'Taufik Nurhidayat', 'taufik@gmail.com', 'taufik2037', 222222, '$2y$10$0sI9oBC9dFgQOCTjs7wx9O1304nLKSYzCkykPXZSDIaiMbqj9CKKS', 2, 1, 1669907396, 'default.jpg');
 
 -- --------------------------------------------------------
 
@@ -78,7 +77,7 @@ INSERT INTO `users` (`id`, `name__user`, `email__user`, `username__user`, `nik__
 --
 
 CREATE TABLE `users_role` (
-  `id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `role` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -86,7 +85,7 @@ CREATE TABLE `users_role` (
 -- Dumping data untuk tabel `users_role`
 --
 
-INSERT INTO `users_role` (`id`, `role`) VALUES
+INSERT INTO `users_role` (`id_role`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Member'),
 (3, 'Petugas');
@@ -99,19 +98,21 @@ INSERT INTO `users_role` (`id`, `role`) VALUES
 -- Indeks untuk tabel `admins`
 --
 ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `id_role` (`id_role`);
 
 --
 -- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_role` (`id_role`);
 
 --
 -- Indeks untuk tabel `users_role`
 --
 ALTER TABLE `users_role`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_role`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -121,19 +122,35 @@ ALTER TABLE `users_role`
 -- AUTO_INCREMENT untuk tabel `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `users_role`
 --
 ALTER TABLE `users_role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `users_role` (`id_role`);
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `users_role` (`id_role`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
