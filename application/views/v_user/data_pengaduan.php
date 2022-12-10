ed14bc67886658279a7312aae626b6a48622ccfd
taufikn2037 <taufik.mesuji@gmail.com>
2022-12-11 05:35:13 +0700

    add edit profil dan ganti password

--- application/views/v_user/data_pengaduan.php
+++ application/views/v_user/data_pengaduan.php
@@ -13,42 +13,42 @@
                     <th>Aksi</th>
                 </tr>
             </thead>
-            <?php $no = 1; ?>
-            <?php foreach ($data_pengaduan as $dp) : ?>
-                <tbody>
-                    <tr class="text-center">
-                        <td><?= $no++; ?></td>
-                        <td><?= $dp['tgl_pengaduan']; ?></td>
-                        <td><?= $dp['isi_laporan']; ?></td>
-                        <td>
-                            <img width="100" src="<?= base_url() ?>assets/uploads/<?= $dp['foto']; ?>" alt="">
-                        </td>
-                        <td>
-                            <?php
-                            if ($dp['status'] == '0') {
-                                echo '<span class="badge badge-secondary">Sedang Diverifikasi</span>';
-                            } elseif ($dp['status'] == 'proses') {
-                                echo '<span class="badge badge-primary">Sedang Diproses</span>';
-                            } elseif ($dp['status'] == 'selesai') {
-                                echo '<span class="badge badge-success">Selesai Dikerjakan</span>';
-                            } elseif ($dp['status'] == 'tolak') {
-                                echo '<span class="badge badge-danger">Pengaduan Ditolak</span>';
-                            } else {
-                                echo '-';
-                            }
-                            ?>
-                        </td>
-                        <td class="text-center">
-                            <a href="<?= base_url('user/pengaduan_detail/' . $dp['id_pengaduan']) ?>" class="btn btn-success mb-1">Detail</a>
-                            <?php if ($dp['status'] == '0') { ?>
-                                <a href="<?= base_url('user/pengaduan_batal/' . $dp['id_pengaduan']) ?>" class="btn btn-danger mb-1">Hapus</a>
-                                <a href="<?= base_url('user/edit/' . $dp['id_pengaduan']) ?>" class="btn btn-warning mb-1">Edit</a>
-                        </td>
-                    <?php } ?>
+            <tbody>
+                <?php $no = 1; ?>
+                <?php foreach ($data_pengaduan as $dp) : ?>
+                <tr class="text-center">
+                    <td><?= $no++; ?></td>
+                    <td><?= $dp['tgl_pengaduan']; ?></td>
+                    <td><?= $dp['isi_laporan']; ?></td>
+                    <td>
+                        <img width="100" src="<?= base_url() ?>assets/uploads/<?= $dp['foto']; ?>" alt="">
+                    </td>
+                    <td>
+                        <?php
+                        if ($dp['status'] == '0') {
+                            echo '<span class="badge badge-secondary">Sedang Diverifikasi</span>';
+                        } elseif ($dp['status'] == 'proses') {
+                            echo '<span class="badge badge-primary">Sedang Diproses</span>';
+                        } elseif ($dp['status'] == 'selesai') {
+                            echo '<span class="badge badge-success">Selesai Dikerjakan</span>';
+                        } elseif ($dp['status'] == 'tolak') {
+                            echo '<span class="badge badge-danger">Pengaduan Ditolak</span>';
+                        } else {
+                            echo '-';
+                        }
+                        ?>
+                    </td>
+                    <td class="text-center">
+                        <a href="<?= base_url('user/pengaduan_detail/' . $dp['id_pengaduan']) ?>" class="btn btn-success mb-1">Detail</a>
+                        <?php if ($dp['status'] == '0') { ?>
+                            <a href="<?= base_url('user/pengaduan_batal/' . $dp['id_pengaduan']) ?>" class="btn btn-danger mb-1">Hapus</a>
+                            <a href="<?= base_url('user/edit/' . $dp['id_pengaduan']) ?>" class="btn btn-warning mb-1">Edit</a>
+                    </td>
+                <?php } ?>
 
-                    </tr>
-                </tbody>
-            <?php endforeach ?>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
         </table>
     </div>
 </div>
